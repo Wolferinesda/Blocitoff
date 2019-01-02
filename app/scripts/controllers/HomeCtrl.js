3d8431844ce6e4bde1395db44ca1f2f5de2ba618
Daniel Halterman <danielhalterman@Daniels-MacBook-Pro.local>
2019-01-02 05:31:55 -0500

    Setting up firebase

---
+++ app/scripts/controllers/HomeCtrl.js
@@ -0,0 +1,44 @@
+(function() {
+  var app = angular.module("sampleApp", ["firebase"])
+
+  app.controller("SampleCtrl", function($scope, $firebaseObject) {
+    var ref = firebase.database().ref();
+    // download the data into a local object
+    $scope.data = $firebaseObject(ref);
+    // putting a console.log here won't work, see below
+  });
+
+  // JavaScript
+  app.controller("MyCtrl", ["$scope", "$firebaseArray",
+    function($scope, $firebaseArray) {
+      var ref = firebase.database().ref();
+      var list = $firebaseArray(ref);
+
+      // add an item
+      list.$add({ foo: "bar" }).then(...);
+
+      // remove an item
+      list.$remove(2).then(...);
+
+      // make the list available in the DOM
+      $scope.list = list;
+    }
+  ]);
+
+  app.controller("MyCtrl", ["$scope", "$firebaseArray",
+    function($scope, $firebaseArray) {
+      var ref = firebase.database().ref();
+      var messagesRef = ref.child("messages");
+      var query = messagesRef.orderByChild("timestamp").limitToLast(10);
+
+      var list = $firebaseArray(query);
+    }
+  ]);
+
+  function HomeCtrl() {
+  }
+
+  angular
+    .module('blocChat')
+    .controller('HomeCtrl', [HomeCtrl]);
+})();
