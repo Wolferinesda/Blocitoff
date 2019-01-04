4e3d2498b68445e069b6a54e919360aa0aee1465
Daniel Halterman <danielhalterman@Daniels-MacBook-Pro.local>
2019-01-03 22:31:53 -0500

    Trying to figure out app error

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -14,7 +14,19 @@
       });
   }
 
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyA4VWKIHkiFsOcdLoFrlgv4MBAvkJfCDrM",
+    authDomain: "bloc-chat-room-25c2c.firebaseapp.com",
+    databaseURL: "https://bloc-chat-room-25c2c.firebaseio.com",
+    projectId: "bloc-chat-room-25c2c",
+    storageBucket: "bloc-chat-room-25c2c.appspot.com",
+    messagingSenderId: "648503535304"
+  };
+  firebase.initializeApp(config);
+
   angular
     .module('Blocitoff', ['ui.router', 'firebase'])
     .config(config);
+    angular.bootstrap(document, ['Blocitoff']);
 })();
--- app/scripts/controllers/HomeCtrl.js
+++ app/scripts/controllers/HomeCtrl.js
@@ -1,8 +1,20 @@
 (function() {
-  function HomeCtrl ($scope, $firebasearray) {
+  function HomeCtrl ($scope, $firebaseArray) {
+    console.log($scope);
+    console.log($firebaseArray);
+    $scope.name = "BLOC"
+
+    var ref = firebase.database().ref();
+    var list = $firebaseArray(ref);
+    list.$add({ foo: "bar" });
+
+    console.log(list);
+
+    $scope.list = list;
+
   }
 
   angular
-      .module('Blocitoff', ["firebase"])
+      .module('Blocitoff')
       .controller('HomeCtrl', ['$scope', '$firebasearray', HomeCtrl]);
 })();
