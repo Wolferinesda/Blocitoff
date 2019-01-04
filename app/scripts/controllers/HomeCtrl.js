7b4ca6ec3c7b567410bf1c095bbabd5bbd8669df
Daniel Halterman <danielhalterman@Daniels-MacBook-Pro.local>
2019-01-04 18:04:28 -0500

    Trying to figure out app error

--- app/scripts/controllers/HomeCtrl.js
+++ app/scripts/controllers/HomeCtrl.js
@@ -14,6 +14,31 @@
 
   }
 
+  var app = angular.module("sampleApp", ["firebase"]);
+
+  app.controller("SampleCtrl", function($scope, $firebaseObject) {
+  var ref = firebase.database().ref();
+  // download the data into a local object
+  $scope.data = $firebaseObject(ref);
+  // putting a console.log here won't work, see below
+});
+
+app.controller("MyCtrl", ["$scope", "$firebaseArray",
+  function($scope, $firebaseArray) {
+    var ref = firebase.database().ref();
+    var list = $firebaseArray(ref);
+
+    // add an item
+    list.$add({ foo: "bar" }).then(...)
+
+    // remove an item
+    list.$remove(2).then(...);
+
+    // make the list available in the DOM
+    $scope.list = list;
+  }
+]);
+
   angular
       .module('Blocitoff')
       .controller('HomeCtrl', ['$scope', '$firebasearray', HomeCtrl]);
